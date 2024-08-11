17379e3c561bb8269e318df796b1b4bfc74509c3
Nathan Lilienthal <nathan@nixpulvis.com>
2024-08-11 13:19:38 -0400

    Show all events

--- examples/journal.rs
+++ examples/journal.rs
@@ -1,22 +1,10 @@
 use eddn::{subscribe, Message, URL};
-use elite_journal::entry::Event;
 
 fn main() {
     for envelop in subscribe(URL) {
         if let Ok(envelop) = envelop {
             match envelop.message {
-                Message::Journal(entry) => match entry.event {
-                    Event::Location(e) => {
-                        dbg!(e);
-                    }
-                    Event::FsdJump(e) => {
-                        dbg!(e);
-                    }
-                    Event::Docked(e) => {
-                        dbg!(e);
-                    }
-                    _ => {}
-                },
+                Message::Journal(entry) => { dbg!(entry); },
                 _ => {}
             }
         }
